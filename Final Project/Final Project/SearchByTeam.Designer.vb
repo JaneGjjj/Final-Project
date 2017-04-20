683fa8ce518b3ea8db6cfd085164496805d9cf76
JANEGJJJ\jguo_ <guoj01@students.ipfw.edu>
2017-04-19 23:34:32 -0400

    added data bound

--- Final Project/Final Project/SearchByTeam.Designer.vb
+++ Final Project/Final Project/SearchByTeam.Designer.vb
@@ -22,32 +22,54 @@ Partial Class SearchByTeam
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.dgvTeamGame = New System.Windows.Forms.DataGridView()
         Me.cboTeam = New System.Windows.Forms.ComboBox()
         Me.label1 = New System.Windows.Forms.Label()
         Me.btnClose = New System.Windows.Forms.Button()
         Me.btnClear = New System.Windows.Forms.Button()
         Me.btnSearch = New System.Windows.Forms.Button()
+        Me.GameInformationDataSet = New Final_Project.GameInformationDataSet()
+        Me.TeamsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TeamsTableAdapter = New Final_Project.GameInformationDataSetTableAdapters.TeamsTableAdapter()
+        Me.GameBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.GameTableAdapter = New Final_Project.GameInformationDataSetTableAdapters.GameTableAdapter()
+        Me.DateDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.LocationDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Team1DataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Team1BestPlaceInWorldCupDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Team2DataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Team2BestPlceInWorldCupDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.HistoryResultBetween2TeamsDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TodayWinnerDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TodayResultDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
         CType(Me.dgvTeamGame, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GameInformationDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TeamsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GameBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'dgvTeamGame
         '
+        Me.dgvTeamGame.AutoGenerateColumns = False
         Me.dgvTeamGame.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvTeamGame.Location = New System.Drawing.Point(110, 151)
+        Me.dgvTeamGame.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.DateDataGridViewTextBoxColumn, Me.LocationDataGridViewTextBoxColumn, Me.Team1DataGridViewTextBoxColumn, Me.Team1BestPlaceInWorldCupDataGridViewTextBoxColumn, Me.Team2DataGridViewTextBoxColumn, Me.Team2BestPlceInWorldCupDataGridViewTextBoxColumn, Me.HistoryResultBetween2TeamsDataGridViewTextBoxColumn, Me.TodayWinnerDataGridViewTextBoxColumn, Me.TodayResultDataGridViewTextBoxColumn})
+        Me.dgvTeamGame.DataSource = Me.GameBindingSource
+        Me.dgvTeamGame.Location = New System.Drawing.Point(31, 151)
         Me.dgvTeamGame.Name = "dgvTeamGame"
         Me.dgvTeamGame.RowTemplate.Height = 28
-        Me.dgvTeamGame.Size = New System.Drawing.Size(689, 252)
+        Me.dgvTeamGame.Size = New System.Drawing.Size(1029, 252)
         Me.dgvTeamGame.TabIndex = 11
         '
         'cboTeam
         '
+        Me.cboTeam.DataSource = Me.TeamsBindingSource
+        Me.cboTeam.DisplayMember = "TeamName"
         Me.cboTeam.FormattingEnabled = True
         Me.cboTeam.Location = New System.Drawing.Point(255, 78)
         Me.cboTeam.Name = "cboTeam"
         Me.cboTeam.Size = New System.Drawing.Size(195, 28)
         Me.cboTeam.TabIndex = 10
-        Me.cboTeam.Text = "Please Select"
         '
         'label1
         '
@@ -60,7 +82,7 @@ Partial Class SearchByTeam
         '
         'btnClose
         '
-        Me.btnClose.Location = New System.Drawing.Point(641, 480)
+        Me.btnClose.Location = New System.Drawing.Point(886, 490)
         Me.btnClose.Name = "btnClose"
         Me.btnClose.Size = New System.Drawing.Size(174, 42)
         Me.btnClose.TabIndex = 8
@@ -85,11 +107,88 @@ Partial Class SearchByTeam
         Me.btnSearch.Text = "Search"
         Me.btnSearch.UseVisualStyleBackColor = True
         '
+        'GameInformationDataSet
+        '
+        Me.GameInformationDataSet.DataSetName = "GameInformationDataSet"
+        Me.GameInformationDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'TeamsBindingSource
+        '
+        Me.TeamsBindingSource.DataMember = "Teams"
+        Me.TeamsBindingSource.DataSource = Me.GameInformationDataSet
+        '
+        'TeamsTableAdapter
+        '
+        Me.TeamsTableAdapter.ClearBeforeFill = True
+        '
+        'GameBindingSource
+        '
+        Me.GameBindingSource.DataMember = "Game"
+        Me.GameBindingSource.DataSource = Me.GameInformationDataSet
+        '
+        'GameTableAdapter
+        '
+        Me.GameTableAdapter.ClearBeforeFill = True
+        '
+        'DateDataGridViewTextBoxColumn
+        '
+        Me.DateDataGridViewTextBoxColumn.DataPropertyName = "Date"
+        Me.DateDataGridViewTextBoxColumn.HeaderText = "Date"
+        Me.DateDataGridViewTextBoxColumn.Name = "DateDataGridViewTextBoxColumn"
+        '
+        'LocationDataGridViewTextBoxColumn
+        '
+        Me.LocationDataGridViewTextBoxColumn.DataPropertyName = "Location"
+        Me.LocationDataGridViewTextBoxColumn.HeaderText = "Location"
+        Me.LocationDataGridViewTextBoxColumn.Name = "LocationDataGridViewTextBoxColumn"
+        '
+        'Team1DataGridViewTextBoxColumn
+        '
+        Me.Team1DataGridViewTextBoxColumn.DataPropertyName = "Team 1"
+        Me.Team1DataGridViewTextBoxColumn.HeaderText = "Team 1"
+        Me.Team1DataGridViewTextBoxColumn.Name = "Team1DataGridViewTextBoxColumn"
+        '
+        'Team1BestPlaceInWorldCupDataGridViewTextBoxColumn
+        '
+        Me.Team1BestPlaceInWorldCupDataGridViewTextBoxColumn.DataPropertyName = "Team 1 Best Place in World Cup"
+        Me.Team1BestPlaceInWorldCupDataGridViewTextBoxColumn.HeaderText = "Team 1 Best Place in World Cup"
+        Me.Team1BestPlaceInWorldCupDataGridViewTextBoxColumn.Name = "Team1BestPlaceInWorldCupDataGridViewTextBoxColumn"
+        '
+        'Team2DataGridViewTextBoxColumn
+        '
+        Me.Team2DataGridViewTextBoxColumn.DataPropertyName = "Team 2"
+        Me.Team2DataGridViewTextBoxColumn.HeaderText = "Team 2"
+        Me.Team2DataGridViewTextBoxColumn.Name = "Team2DataGridViewTextBoxColumn"
+        '
+        'Team2BestPlceInWorldCupDataGridViewTextBoxColumn
+        '
+        Me.Team2BestPlceInWorldCupDataGridViewTextBoxColumn.DataPropertyName = "Team 2 Best Plce in World Cup"
+        Me.Team2BestPlceInWorldCupDataGridViewTextBoxColumn.HeaderText = "Team 2 Best Plce in World Cup"
+        Me.Team2BestPlceInWorldCupDataGridViewTextBoxColumn.Name = "Team2BestPlceInWorldCupDataGridViewTextBoxColumn"
+        '
+        'HistoryResultBetween2TeamsDataGridViewTextBoxColumn
+        '
+        Me.HistoryResultBetween2TeamsDataGridViewTextBoxColumn.DataPropertyName = "History Result between 2 teams"
+        Me.HistoryResultBetween2TeamsDataGridViewTextBoxColumn.HeaderText = "History Result between 2 teams"
+        Me.HistoryResultBetween2TeamsDataGridViewTextBoxColumn.Name = "HistoryResultBetween2TeamsDataGridViewTextBoxColumn"
+        '
+        'TodayWinnerDataGridViewTextBoxColumn
+        '
+        Me.TodayWinnerDataGridViewTextBoxColumn.DataPropertyName = "Today winner"
+        Me.TodayWinnerDataGridViewTextBoxColumn.HeaderText = "Today winner"
+        Me.TodayWinnerDataGridViewTextBoxColumn.Name = "TodayWinnerDataGridViewTextBoxColumn"
+        '
+        'TodayResultDataGridViewTextBoxColumn
+        '
+        Me.TodayResultDataGridViewTextBoxColumn.DataPropertyName = "Today Result"
+        Me.TodayResultDataGridViewTextBoxColumn.HeaderText = "Today Result"
+        Me.TodayResultDataGridViewTextBoxColumn.Name = "TodayResultDataGridViewTextBoxColumn"
+        '
         'SearchByTeam
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(900, 588)
+        Me.ClientSize = New System.Drawing.Size(1115, 588)
         Me.Controls.Add(Me.dgvTeamGame)
         Me.Controls.Add(Me.cboTeam)
         Me.Controls.Add(Me.label1)
@@ -99,6 +198,9 @@ Partial Class SearchByTeam
         Me.Name = "SearchByTeam"
         Me.Text = "Search Game By Team"
         CType(Me.dgvTeamGame, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GameInformationDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TeamsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GameBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -110,4 +212,18 @@ Partial Class SearchByTeam
     Private WithEvents btnClose As Button
     Private WithEvents btnClear As Button
     Private WithEvents btnSearch As Button
+    Friend WithEvents GameInformationDataSet As GameInformationDataSet
+    Friend WithEvents TeamsBindingSource As BindingSource
+    Friend WithEvents TeamsTableAdapter As GameInformationDataSetTableAdapters.TeamsTableAdapter
+    Friend WithEvents GameBindingSource As BindingSource
+    Friend WithEvents GameTableAdapter As GameInformationDataSetTableAdapters.GameTableAdapter
+    Friend WithEvents DateDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents LocationDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Team1DataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Team1BestPlaceInWorldCupDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Team2DataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Team2BestPlceInWorldCupDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents HistoryResultBetween2TeamsDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents TodayWinnerDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents TodayResultDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
 End Class
--- Final Project/Final Project/SearchByTeam.vb
+++ Final Project/Final Project/SearchByTeam.vb
@@ -4,6 +4,10 @@
     End Sub
 
     Private Sub SearchByTeam_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'GameInformationDataSet.Game' table. You can move, or remove it, as needed.
+        Me.GameTableAdapter.Fill(Me.GameInformationDataSet.Game)
+        'TODO: This line of code loads data into the 'GameInformationDataSet.Teams' table. You can move, or remove it, as needed.
+        Me.TeamsTableAdapter.Fill(Me.GameInformationDataSet.Teams)
 
         cboTeam.SelectedIndex = -1
     End Sub
